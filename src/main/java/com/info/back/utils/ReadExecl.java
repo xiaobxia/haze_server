09e8ca81b0ee2467121d65247216db0dc68035f1
xiaobxia <673806687@qq.com>
2019-05-31 13:46:11 +0800

    修改数据库地址

--- src/main/java/com/info/back/utils/ReadExecl.java
+++ src/main/java/com/info/back/utils/ReadExecl.java
@@ -63,11 +63,11 @@ public class ReadExecl {
 
 		//把spring文件上传的MultipartFile转换成CommonsMultipartFile类型
 		CommonsMultipartFile cf= (CommonsMultipartFile)Mfile; //获取本地存储路径
-		File file = new  File("G:\\fileupload");
+		File file = new  File("D:\\fileupload");
 		//创建一个目录 （它的路径名由当前 File 对象指定，包括任一必须的父路径。）
 		if (!file.exists()) file.mkdirs();
 		//新建一个文件
-		File file1 = new File("G:\\fileupload" + new Date().getTime() + ".xlsx");
+		File file1 = new File("D:\\fileupload" + new Date().getTime() + ".xlsx");
 		//将上传的文件写入新建的文件中
 		try {
 			cf.getFileItem().write(file1);
